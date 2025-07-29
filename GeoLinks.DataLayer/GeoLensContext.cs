82be12359c302787aeda67abac9d7a5199967c58
manoharr180 <manoharr180@example.com>
2025-07-29 20:39:20 +0530

    added Order endpoint

--- GeoLinks.DataLayer/GeoLensContext.cs
+++ GeoLinks.DataLayer/GeoLensContext.cs
@@ -16,28 +16,36 @@ namespace GeoLinks.DataLayer
         public DbSet<LoginUser> LoginUsers { get; set; }
         public DbSet<StoreDto> stores { get; set; }
         public DbSet<StoreItemDetailsDto> CartItems { get; set; }
+        public DbSet<OrderDto> Orders { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<LoginUser>()
                 .HasNoKey();
+
             modelBuilder.Entity<StoreDto>()
                 .HasKey(s => s.StoreId);
             modelBuilder.Entity<StoreDto>()
                 .Property(s => s.StoreId)
                 .ValueGeneratedOnAdd();
 
-            // Ensure StoreItemDetailsDto has a primary key
             modelBuilder.Entity<StoreItemDetailsDto>()
                 .HasKey(s => s.ItemId);
             modelBuilder.Entity<StoreItemDetailsDto>()
                 .Property(s => s.ItemId)
                 .ValueGeneratedOnAdd();
-    
+
             modelBuilder.Entity<StoreItemDetailsDto>()
                 .HasOne(sid => sid.Store)
                 .WithMany(s => s.StoreItemDetails)
                 .HasForeignKey(sid => sid.StoreId)
                 .HasPrincipalKey(s => s.StoreId);
+
+            // Add OrderDto configuration
+            modelBuilder.Entity<OrderDto>()
+                .HasKey(o => o.OrderId);
+            modelBuilder.Entity<OrderDto>()
+                .Property(o => o.OrderId)
+                .ValueGeneratedOnAdd();
         }
     }
 }
