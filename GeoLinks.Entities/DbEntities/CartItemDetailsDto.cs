ee4021f99b5bbea801a5178a7bc122717a7c767f
manoharr180 <manoharr180@example.com>
2025-08-16 08:34:42 +0530

    included userid

--- GeoLinks.Entities/DbEntities/CartItemDetailsDto.cs
+++ GeoLinks.Entities/DbEntities/CartItemDetailsDto.cs
@@ -1,8 +1,11 @@
+using System;
+
 public class CartItemDetailsDto
 {
     public string StoreId { get; set; }
     public int ItemId { get; set; }
     public int CartItemId { get; set; }
+    public int UserId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string Price { get; set; }
